a5d377677306f6753ecdd08ef660ee47374dd120
tiger221 <55827886+tiger221@users.noreply.github.com>
2019-10-19 23:07:14 +0530

    Update Question1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,11 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     # modify and then return the variable below
-    answer = -5
+    count = 0
+    answer = initialLevelOfDebt*(repaymentPercentage/100)
+    while(initialLevelOfDebt>50):
+        initialLevelOfDebt = initialLevelOfDebt + initialLevelOfDebt*(interestPercentage/100)
+        initialLevelOfDebt = initialLevelOfDebt - 100
+        count = count + 1
+    answer = answer + count * 100 + initialLevelOfDebt + initialLevelOfDebt*(interestPercentage/100)
     return answer
